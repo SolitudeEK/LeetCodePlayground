4c02e0faed9646a643498e37ece01ab7cd28026a
Egor Knyazev <egorknz@gmail.com>
2024-08-07 17:17:54 +0200

    Add NumberToWords

---
+++ LeetCodePlayground/HardProblems/IntToEnglish.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePlayground.HardProblems
+{
+    /// <summary>
+    /// The class for problem 273. Integer To English Words
+    /// </summary>
+    public class IntToEnglish
+    {
+        public string NumberToWords(int num)
+        {
+            string[] map = new string[]
+            {
+                "Zero", "One", "Two", "Three", "Four", "Five", "Six",
+                "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
+                "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
+                "Eighteen", "Nineteen"
+            };
+            string[] map2 = new string[]
+            {
+                "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty",
+                "Seventy", "Eighty", "Ninety"
+            };
+            if (num == 0)
+                return "Zero";
+
+            List<string> words = new List<string>();
+            void PartToWord(int num)
+            {
+                if (num > 99)
+                {
+                    words.Add(map[num / 100]);
+                    words.Add("Hundred");
+                    num = num % 100;
+                }
+                if (num > 19)
+                {
+                    words.Add(map2[num / 10]);
+                    if (num % 10 != 0)
+                        words.Add(map[num % 10]);
+                }
+                else if (num > 0)
+                    words.Add(map[num]);
+            }
+
+            int i = (int)Math.Floor(Math.Log10(num)+1);
+            if (i > 9)
+            {
+                PartToWord(num / 1_000_000_000);
+                words.Add("Billion");
+                num %= 1_000_000_000;
+            }
+            if(i > 6 && num / 1_000_000 != 0)
+            {
+                PartToWord(num / 1_000_000);
+                words.Add("Million");
+                num %= 1_000_000;
+            }
+            if (i > 3 && num / 1_000 != 0)
+            {
+                PartToWord(num / 1_000);
+                words.Add("Thousand");
+                num %= 1_000;
+            }
+            PartToWord(num);
+
+            return String.Join(' ', words);
+        }
+    }
+}
--- LeetCodePlayground/Program.cs
+++ LeetCodePlayground/Program.cs
@@ -1,10 +1,12 @@
 ﻿using LeetCodePlayground;
+using LeetCodePlayground.HardProblems;
 using System.Diagnostics;
 public class Programm
 {
     public static void Main()
     {
         Solution a = new Solution();
+        IntToEnglish b = new IntToEnglish();
         Stopwatch timer = new Stopwatch();
         int[][] data =
 {
@@ -19,7 +21,8 @@ public class Programm
 
 
         timer.Start();
-        var ans = a.MinimumPushes("aabbccddeeffgghhiiiiii");
+        //var ans = a.MinimumPushes("aabbccddeeffgghhiiiiii");
+        var ans = b.NumberToWords(1234567);
         timer.Stop();
         Console.WriteLine($"Answer: {ans}");
         Console.WriteLine($"Time taken: {timer.ElapsedMilliseconds} ms");
